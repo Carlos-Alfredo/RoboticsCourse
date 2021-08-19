a8ba821987e284f78c0fd5d1c718ee3d3a2792b1
CarlosAlfredo <56643645+Carlos-Alfredo@users.noreply.github.com>
2021-08-19 11:19:17 -0300

    Mudar os L's e o vetor desejado

--- code/inverse_kin.py
+++ code/inverse_kin.py
@@ -9,13 +9,15 @@ q_1 =opti.variable();q_2 =opti.variable();q_3 =opti.variable();
 
 opti.set_initial(q_1,-30.91);opti.set_initial(q_2,-52.42);opti.set_initial(q_3,-72.68)
 
-theta = [0.1135446771428052, 0.1552258044760527, 0.14380575473898485, -0.375]
+#theta = [0.1135446771428052, 0.1552258044760527, 0.14380575473898485, -0.375]
+theta = [0.1552, 0.1368, 0.1917,0]
 
 
 f = directf(np.array([q_1,q_2,q_3]),theta)
 
 
-p_desire = np.array([-0.3100309371948242, 0.19369974732398987, -0.036061882972717285])
+#p_desire = np.array([-0.3100309371948242, 0.19369974732398987, -0.036061882972717285])
+p_desire = np.array([0.353976309299469, 0.16158318519592285, -0.04253572225570679])
 
 opti.minimize(np.sqrt((p_desire[0]-f[0])**2+(p_desire[1]-f[1])**2+(p_desire[2]-f[2])**2))
 opti.solver('ipopt');
